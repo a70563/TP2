13a71f54d2f898e85d6b684ef38b37c620b5d30c
André Vieira <a64108@ualg.pt>
2023-11-26 16:52:55 +0000

    corrigido o erro io_terminal.py onde tinhauma funçao redundante do io_ficheiros

--- io_terminal.py
+++ io_terminal.py
@@ -63,13 +63,3 @@ def guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de
     :param lista_de_veiculos: lista de veiculos registados e guardados
     :param lista_de_faturas: lista de faturas feitas e guardadas
     """
-
-    op = input("Os dados nos ficheiros serão sobrepostos. Continuar (s/N)?")
-    if op in ['s', 'S']:
-        guarda_em_ficheiro(nome_ficheiro_lista_de_veiculos, lista_de_veiculos)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_clientes, lista_de_clientes)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_faturas, lista_de_faturas)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_items, lista_de_items)
-        guarda_em_ficheiro(nome_ficheiro_lista_de_cortesia, lista_de_cortesia)
-    else:
-        print("Gravação cancelada...")
